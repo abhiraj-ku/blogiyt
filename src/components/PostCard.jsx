1a62d91ead162fd3655e5c3ae16fdef03d12e693
abhishek <abhirajabhi312@gmail.com>
2023-09-29 17:30:02 +0530

    added select/dropdown  option

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,20 @@
+import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
+
+function PostCard({ $id, title, featuredImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 rounded-lg">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+          />
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
