0ba7d51bf98f11ad8d4d1bc802d3362d4b17858e
abhishek <abhirajabhi312@gmail.com>
2023-09-29 22:29:10 +0530

    added login and signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,32 +1,33 @@
 import { useState } from "react";
+import authService from "../appwrite/auth";
 import { Link, useNavigate } from "react-router-dom";
-import { login as authLogin } from "../store/authSlice";
+import { login } from "../store/authSlice";
 import { Button, Logo, Input } from "./index";
 import { useDispatch } from "react-redux";
-import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
 
-function Login() {
+function Signup() {
   const navigate = useNavigate();
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
-  const [error, setError] = useState("");
 
-  const login = async (data) => {
+  const create = async (data) => {
     setError("");
     try {
-      const session = await authService.login(data);
-      if (session) {
+      const userData = await authService.createAccount(data);
+      if (userData) {
         const userData = await authService.getCurrentUser();
-        if (userData) dispatch(authLogin(userData));
-        navigate("/"); //naviagte is used to automatically sends to that location but Link is just like <a> tag , you have to click it
+        if (userData) dispatch(login(userData));
+        navigate("/");
       }
     } catch (error) {
       setError(error.message);
     }
   };
+
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -36,20 +37,28 @@ function Login() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
+            <Input
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
             <Input
               label="Email: "
               placeholder="Enter your email"
@@ -72,7 +81,7 @@ function Login() {
               })}
             />
             <Button type="submit" className="w-full">
-              Sign in
+              Create Account
             </Button>
           </div>
         </form>
@@ -81,4 +90,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./containers/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Buttons.jsx";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button };
